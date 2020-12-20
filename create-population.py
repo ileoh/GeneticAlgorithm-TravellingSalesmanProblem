6e59720dd9196b143422ac64494f7c5b73082b5b
Leonardo Pena <leo94pena@gmail.com>
2020-12-20 02:01:52 -0300

    Translating the comments

--- create-population.py
+++ create-population.py
@@ -3,21 +3,21 @@ Here we can create the class City and the class Fitness
 Then we are going to create the initial population
 '''
 
-# Classe City para criar e lidar com as cidades
+# City class to create and deal with cities
     class City:
-    # Inicialmente possui apenas as coordenadas x e y
+    # Initially only has x and y coordinates
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    # Com o teorema Pythagorean, calculamos a distancia
+    # With the Pythagorean theorem, we calculate the distance
     def distance(self, city):
         xDis = abs(self.x - city.x)
         yDis = abs(self.y - city.y)
         distance = np.sqrt((xDis ** 2) + (yDis ** 2))
         return distance
 
-    # Modo limpo mostrar a saída das cidades como coordendas
+    # Clean mode to show the cities output as coordinates
     def __repr__(self):
         return "(" + str(self.x) + "," + str(self.y) + ")"
 
@@ -26,7 +26,7 @@ Then we are going to create the initial population
         coord = [self.x, self.y]
         return coord
 
-# Classe Fitness, no caso vamos tratar o fitness como o inverso da distancia da rota
+# Fitness Class, in this case we will treat fitness as the inverse of the route distance
 class Fitness:
     def __init__(self, route):
         self.route = route
@@ -39,7 +39,7 @@ class Fitness:
             for i in range(0, len(self.route)):
                 fromCity = self.route[i]
                 toCity = None
-                if i + 1 < len(self.route):  # Calculo para comer e terminar no mesmo local
+                if i + 1 < len(self.route):  # I calculate to start and end in the same place
                     toCity = self.route[i + 1]
                 else:
                     toCity = self.route[0]
@@ -52,12 +52,12 @@ class Fitness:
             self.fitness = 1 / float(self.routeDistance())
         return self.fitness
 
-## CRIAÇÃO DA POPULAÇÃO: createRoute(individuo), initialPopulation(populacao)
+## CREATING THE POPULATION: createRoute (individual), initialPopulation (population)
 def createRoute(cityList):
     # route = random.sample(cityList, len(cityList))
-    teste_rota = random.sample(cityList[1:], len(cityList[1:]))  # Aleatoriza as cidades q nao o deposito
-    teste_rota.insert(0, cityList[0])  # insere o deposito no inicio da rota
-    teste_rota.insert(len(teste_rota), cityList[0])  # insere o deposito no fim da rota
+    teste_rota = random.sample(cityList[1:], len(cityList[1:]))  # Randomize the cities that are not the deposit
+    teste_rota.insert(0, cityList[0])  # inserts the deposit at the beginning of the rotation
+    teste_rota.insert(len(teste_rota), cityList[0])  # inserts the deposit at the end of the route
     return teste_rota
     # return route
 def initialPopulation(popSize, cityList):
